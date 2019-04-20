ad8e45a8060377a5dc7cefe1e0cf51f35c52a3e8
Cindy Zhang <czhang762@gmail.com>
2019-04-20 19:25:55 +0000

    Created new changes to kids_safe

---
+++ kidssafe.py
@@ -0,0 +1,10 @@
+ def kids_safe():
+     count = 0
+     swear_words_count = 0
+     with open(file.txt, 'r') as file1
+        for word in file1:
+            count += 1
+            if word in swear_words:
+                swear_words_count += 1
+                
+
