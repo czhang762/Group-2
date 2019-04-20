f8f81886a67e61ddcf376f8233190b13981c9f52
Cindy Zhang <czhang762@gmail.com>
2019-04-20 19:39:28 +0000

    Changed the kids safe function

--- kidssafe.py
+++ kidssafe.py
@@ -1,10 +1,24 @@
  def kids_safe():
-     count = 0
-     swear_words_count = 0
-     with open(file.txt, 'r') as file1
-        for word in file1:
-            count += 1
-            if word in swear_words:
-                swear_words_count += 1
-                
+     import os
+     import csv
+
+     directory = "/Users/ChristineRothacker/Desktop/Lyrics/271~E-40~Neva-Broke.txt"
+     curse_path = "/Users/ChristineRothacker/Desktop/cursewords.txt"
+     
+     a=open(curse_path, "r")
+     curse_words=a.read().lower().split()
+     print(curse_words)
+     f = open(directory, "r")
+     contents=f.read().lower().split()
+     #print(contents)
+     curse_count=0
+     
+     for i in contents:
+         if i in curse_words:
+             curse_count +=1.0
+             if i.__contains__('*'):
+                 curse_count +=1.0
+                 print(curse_count)
+                 print(curse_count/len(contents))
+
 
