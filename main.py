c19dbce62b4fa6eb8e8dd89dccaf5bef9367a095
crr2153 <47306262+crr2153@users.noreply.github.com>
2019-05-05 17:59:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -182,8 +182,20 @@ class Song:
 
 	##creates and returns a dictionary with all of the characterizations
 	def to_dict(self, song_info):
-		song_dict={"id":song_info[0],"artist":song_info[1],"title":song_info[2],"kid_safe":self.kid_safe_score,"love":self.love_score,"mood":self.mood_score,"length":self.length_score,"complexity":self.complexity_score}
-		return song_dict
+		od=OrderedDict()
+        	od['id:']=song_info[0]
+		od['artist:']=song_info[1]
+		od['title:']=song_info[2]
+		od['kid_safe:']=self.kid_safe_score
+		od['love:']=self.love_score
+		od['mood:']=self.mood_score
+		od['length:']=self.length_score
+		od['complexity:']=self.complexity_score
+		#song_dict([("id",song_info[0]),("artist",song_info[1]),("title",song_info[2]),("kid_safe",self.kid_safe_score),("love",self.love_score),("mood",self.mood_score),("length",self.length_score),("complexity",self.complexity_score)])
+		#song_dict={}
+		#song_dict={"id":song_info[0],"artist":song_info[1],"title":song_info[2],"kid_safe":self.kid_safe_score,"love":self.love_score,"mood":self.mood_score,"length":self.length_score,"complexity":self.complexity_score}
+		#song_ordered_dict=collections.OrderedDict(song_dict)
+		return od
 
 Final_Song_Dictionary={}
 List_of_Song_Dictionaries=[]
