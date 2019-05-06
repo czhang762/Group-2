2b9a11e0a73f87c34f8c2a869f6e219cf3d8edde
crr2153 <47306262+crr2153@users.noreply.github.com>
2019-05-06 19:05:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -198,25 +198,26 @@ class Song:
 		#song_dict={"id":song_info[0],"artist":song_info[1],"title":song_info[2],"kid_safe":self.kid_safe_score,"love":self.love_score,"mood":self.mood_score,"length":self.length_score,"complexity":self.complexity_score}
 		#song_ordered_dict=collections.OrderedDict(song_dict)
 		return od
+def main()
+	Final_Song_Dictionary={}
+	List_of_Song_Dictionaries=[]
+	for filename in os.listdir(args.indir):
+		if filename.endswith(".txt"):
+			song_info_from_title=get_name_info(filename) 
+			song_path=args.indir+"/"+filename
+			song_size=os.stat(song_path).st_size
+			with open(song_path) as song_file:
+				song_lyrics=song_file.read().lower()
+				song_lyrics=song_lyrics.replace('-',' ')
+				song_lyrics=re.sub(r'[^\w\s]','',song_lyrics)
+				language=detect(song_lyrics) 
+				if(language=='en'):
+					curr_song=Song(song_lyrics, song_size)
+					current_song_dict=curr_song.to_dict(song_info_from_title)
+					List_of_Song_Dictionaries.append(current_song_dict)
 
-Final_Song_Dictionary={}
-List_of_Song_Dictionaries=[]
-for filename in os.listdir(args.indir):
-	if filename.endswith(".txt"):
-		song_info_from_title=get_name_info(filename) 
-		song_path=args.indir+"/"+filename
-		song_size=os.stat(song_path).st_size
-		with open(song_path) as song_file:
-			song_lyrics=song_file.read().lower()
-			song_lyrics=song_lyrics.replace('-',' ')
-			song_lyrics=re.sub(r'[^\w\s]','',song_lyrics)
-			language=detect(song_lyrics) 
-			if(language=='en'):
-				curr_song=Song(song_lyrics, song_size)
-				current_song_dict=curr_song.to_dict(song_info_from_title)
-				List_of_Song_Dictionaries.append(current_song_dict)
-
-Final_Song_Dictionary={"characterizations":List_of_Song_Dictionaries}
-print(Final_Song_Dictionary)
+	Final_Song_Dictionary={"characterizations":List_of_Song_Dictionaries}
+	return Final_Song_Dictionary
+print(main())
 
 
