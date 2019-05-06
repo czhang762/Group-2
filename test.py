f18933c83f5f4cd805c76da1bc250aef64921437
czhang762 <48304422+czhang762@users.noreply.github.com>
2019-05-05 22:34:35 -0400

    Update test.py
    edit the test functions

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import unittest
 
 class LoveTestCase(unittest.TestCase):
-    def test_love_score(self):
+    def test_not_love_song(self):
         lyrics = 'I love you'
         self.assertEqual(love(lyrics), 0)
         
-    def test_love_score(self):
+    def test_love_score_0.2(self):
         lyrics = 'Love love love heart and sometimes it was not there. My affection for my lover or my wife died'
         self.assertEqual(love(lyrics), 0.2)
         
-    def test_love_score(self):
+    def test_love_score_0.4(self):
         lyrics = "love heart loved cherish adore lover wife unconditional affection amore enchant muse relationship seduce"
         self.assertEqual(love(lyrics),0.4)
         
-    def test_love_score(self):
+    def test_love_score_0.6(self):
         lyrics = "love heart loved cherish adore lover wife unconditional affection amore enchant muse relationship seduce relationships darling dear love heart loved cherish adore lover wife unconditional affection amore enchant muse"
         self.assertEqual(love(lyrics),0.6)
         
-    def test_love_score(self):
+    def test_love_score_0.8(self):
         lyrics = "love heart loved cherish adore lover wife unconditional affection amore enchant muse relationship seduce relationships darling dear love heart loved cherish adore lover wife unconditional affection amore enchant muse relationship seduce relationships darling dear"
         self.assertEqual(love(lyrics),0.8)
         
